5c67b7d8d0e3b2890fd8582d80d9670896f0f992
en20 <enzolozano90@gmail.com>
2024-04-17 16:32:22 -0300

    feat: adicionar responsividade a homepage

--- FrontEnd-React/src/pages/Home/Home.tsx
+++ FrontEnd-React/src/pages/Home/Home.tsx
@@ -8,10 +8,11 @@ const Home: React.FC = () => {
         <Layout>
             <section className={styles.container}>
             <div className={styles.img}>
-            <img src="/images/home.svg" alt="Logo" width={520} className={styles.logo} />
+            <img src="/images/home.svg" alt="Logo" width={520} className={styles.img} />
             </div>
             <div className={styles.text}>
-                <p>Conecte - se as vagas mais atuais do mercado!</p>
+                <p>Conecte - se as vagas mais atuais do mercado! </p>
+            
             </div>
             </section>
         </Layout>
